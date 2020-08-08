876c1decf6ae359b9ad7157e4c0165f5da753779
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-08 20:18:04 -0300

    Update HomeScreen

--- screens/Discover/index.js
+++ screens/Discover/index.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 
 export default function DiscoverScreen() {
   return (
-    <View>
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
       <Text>Hello, I am your Discover</Text>
     </View>
   );
--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Hello, I am your home</Text>
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <Text>
+        Hello, I am your home
+        {[1, 2, 3, 4, 5].map((item) => {
+          return (
+            <RectButton>
+              <Text>Podcast: 0:{item}</Text>
+            </RectButton>
+          );
+        })}
+      </Text>
     </View>
   );
 }
